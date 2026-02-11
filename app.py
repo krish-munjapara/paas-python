af0ab9fd74e3de1e7e5b216e56bce8148a4a7a72
krish munjapara <munjaparakrish09@gmail.com>
2026-02-11 13:36:19 +0530

    Set up Flask application with home route

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello from Public PaaS")
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Public PaaS"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
